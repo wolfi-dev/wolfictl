01798da5e56a466df67b63494be1a72a0c0d938a
James Rawlings <jrawlings@chainguard.dev>
2022-11-29 15:19:38 +0000

    add a test to make sure ICU versions parse correctly as seeing an issue with the generated melange bump
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

--- pkg/update/monitorService_test.go
+++ pkg/update/monitorService_test.go
@@ -22,12 +22,25 @@ func TestMonitorService_parseData(t *testing.T) {
 }
 
 func TestMonitorService_parseVersions(t *testing.T) {
-	data, err := os.ReadFile(filepath.Join("testdata", "versions.json"))
-	assert.NoError(t, err)
 
 	m := MonitorService{Logger: log.New(log.Writer(), "test: ", log.LstdFlags|log.Lmsgprefix)}
-	version, err := m.parseVersions(data)
 
-	assert.NoError(t, err)
-	assert.Equal(t, "2.3.1", version)
+	tests := []struct {
+		name                  string
+		expectedLatestVersion string
+	}{
+		{name: "versions", expectedLatestVersion: "2.3.1"},
+		{name: "icu_versions", expectedLatestVersion: "72-1"},
+	}
+	for _, tt := range tests {
+		data, err := os.ReadFile(filepath.Join("testdata", tt.name+".json"))
+		assert.NoError(t, err)
+
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := m.parseVersions(data)
+			assert.NoError(t, err)
+			assert.Equalf(t, tt.expectedLatestVersion, got, "parseVersions(%v)", tt.name)
+			assert.Equalf(t, tt.expectedLatestVersion, got, "parseVersions(%v)", tt.name)
+		})
+	}
 }
