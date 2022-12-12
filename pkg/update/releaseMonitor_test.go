ce4b83adc81b0a1058a0d8e12320126a661f5370
James Rawlings <jrawlings@chainguard.dev>
2022-12-12 18:00:27 +0000

    remove old test that was used for early dev but moved to proper unit test
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

--- pkg/update/releaseMonitor_test.go
+++ pkg/update/releaseMonitor_test.go
@@ -1,15 +1,10 @@
 package update
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"testing"
-
-	"github.com/shurcooL/githubv4"
-	"golang.org/x/oauth2"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -36,29 +31,4 @@ func TestReleaseMonitor_parseVersions(t *testing.T) {
 			assert.Equalf(t, tt.expectedLatestVersion, got, "parseVersions(%v)", tt.name)
 		})
 	}
-}
-
-func TestMonitorService_getGet(t *testing.T) {
-	src := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
-	)
-	httpClient := oauth2.NewClient(context.Background(), src)
-	client := githubv4.NewClient(httpClient)
-
-	// Query details about a GitHub repository releases
-
-	var q struct {
-		Search `graphql:"search(first: $count, query: $searchQuery, type: REPOSITORY)"`
-	}
-	variables := map[string]interface{}{
-		"searchQuery": githubv4.String(fmt.Sprintf(`repo:%s repo:%s`, githubv4.String("jenkinsci/jenkins"), githubv4.String("sigstore/cosign"))),
-		"count":       githubv4.Int(100),
-		"first":       githubv4.Int(5),
-	}
-
-	err := client.Query(context.Background(), &q, variables)
-	assert.NoError(t, err)
-
-	printJSON(q)
-
 }
