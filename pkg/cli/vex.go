9364dfe924ac4c80484492f523008fab1eb634a1
Jon Johnson <jon.johnson@chainguard.dev>
2023-06-07 09:19:16 -0700

    Drop wolfictl vex
    The vex command depends on things within melange that have been
    removed, and we no longer use the vex command ourselves, so we have to
    drop it in order to bump melange.
    
    Signed-off-by: Jon Johnson <jon.johnson@chainguard.dev>

--- pkg/cli/vex.go
+++ pkg/cli/vex.go
@@ -1,19 +1,16 @@
 package cli
 
 import (
-	"fmt"
-	"os"
+	"log"
 
-	"chainguard.dev/melange/pkg/build"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/wolfi-dev/wolfictl/pkg/vex"
 )
 
 func VEX() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "vex",
-		Short: "Tools to generate VEX statements for Wolfi packages and images",
+		Deprecated: "This command does nothing, and will be revmoved in a future version.",
+		Use:        "vex",
+		Short:      "Tools to generate VEX statements for Wolfi packages and images",
 		Long: `wolfictl vex: Tools to generate VEX statements for Wolfi packages and images
 		
 The vex family of subcommands interacts with Wolfi data and configuration
@@ -42,13 +39,6 @@ more about the VEX tooling powering wolfictl see: https://openvex.dev/
 	return cmd
 }
 
-var vexCfg = vex.Config{
-	Distro:     "wolfi",
-	DistroRepo: "",
-	Author:     "",
-	AuthorRole: "",
-}
-
 func addPackage(parent *cobra.Command) {
 	cmd := &cobra.Command{
 		Use:           "package [flags] CONFIG [CONFIG]...",
@@ -56,29 +46,7 @@ func addPackage(parent *cobra.Command) {
 		Short:         "Generate a VEX document from package configuration files",
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) == 0 {
-				cmd.Help() //nolint:errcheck
-				return errors.New("too few arguments")
-			}
-
-			confs := []*build.Configuration{}
-			for _, configPath := range args {
-				buildCfg, err := build.ParseConfiguration(configPath)
-				if err != nil {
-					return err
-				}
-				confs = append(confs, buildCfg)
-			}
-
-			doc, err := vex.FromPackageConfiguration(vexCfg, confs...)
-			if err != nil {
-				return fmt.Errorf("creating VEX document: %w", err)
-			}
-
-			if err := doc.ToJSON(os.Stdout); err != nil {
-				return fmt.Errorf("marshaling VEX document: %w", err)
-			}
-
+			log.Print("Did nothing!")
 			return nil
 		},
 	}
@@ -110,29 +78,18 @@ or, if not specified, it will clone the repo for you.
 `,
 		SilenceErrors: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 1 {
-				cmd.Help() //nolint:errcheck
-				return errors.New("required parameter missing: path to SBOM")
-			}
-
-			doc, err := vex.FromSBOM(vexCfg, args[0])
-			if err != nil {
-				return fmt.Errorf("creating VEX document from SBOM: %w", err)
-			}
-
-			if err := doc.ToJSON(os.Stdout); err != nil {
-				return fmt.Errorf("marshaling VEX document")
-			}
-
+			log.Print("Did nothing!")
 			return nil
 		},
 	}
 	addCommonVexFlags(cmd)
-	cmd.Flags().StringVar(&vexCfg.DistroRepo, "repo", "", "path to a local clone of the wolfi-dev/os repo")
+	var s string
+	cmd.Flags().StringVar(&s, "repo", "", "path to a local clone of the wolfi-dev/os repo")
 	parent.AddCommand(cmd)
 }
 
 func addCommonVexFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&vexCfg.Author, "author", "", "author of the VEX document")
-	cmd.Flags().StringVar(&vexCfg.AuthorRole, "role", "", "role of the author of the VEX document")
+	var s string
+	cmd.Flags().StringVar(&s, "author", "", "author of the VEX document")
+	cmd.Flags().StringVar(&s, "role", "", "role of the author of the VEX document")
 }
