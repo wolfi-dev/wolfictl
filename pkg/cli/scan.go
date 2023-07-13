9ce6c8b0265aefb0b975e140a36b7854cfc9cb0f
Dan Luhring <dluhring@chainguard.dev>
2023-07-13 08:33:31 -0400

    Update UI and allow multiple apks
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- pkg/cli/scan.go
+++ pkg/cli/scan.go
@@ -3,41 +3,45 @@ package cli
 import (
 	"fmt"
 	"os"
+	"path"
+	"sort"
 	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/samber/lo"
+	"github.com/savioxavier/termlink"
 	"github.com/spf13/cobra"
 	"github.com/wolfi-dev/wolfictl/pkg/scan"
 )
 
 func Scan() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "scan <path/to/apk>",
+		Use:   "scan <path/to/package.apk> ...",
 		Short: "Scan an apk file for vulnerabilities",
-		Args:  cobra.ExactArgs(1), // TODO: support scanning multiple apks!
+		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			apkFilePath := args[0]
+			for _, arg := range args {
+				apkFilePath := arg
+				apkFile, err := os.Open(apkFilePath)
+				if err != nil {
+					return fmt.Errorf("failed to open apk file: %w", err)
+				}
 
-			apkFile, err := os.Open(apkFilePath)
-			if err != nil {
-				return fmt.Errorf("failed to open apk file: %w", err)
-			}
-			defer apkFile.Close()
+				fmt.Println(path.Base(apkFilePath))
 
-			findings, err := scan.APK(apkFile)
-			if err != nil {
-				return err
-			}
+				findings, err := scan.APK(apkFile)
+				if err != nil {
+					return err
+				}
 
-			if len(findings) == 0 {
-				fmt.Println("No vulnerabilities found")
-			} else {
-				lineItems := lo.Map(findings, func(f *scan.Finding, _ int) string {
-					return renderFinding(*f)
-				})
+				apkFile.Close()
 
-				fmt.Println(strings.Join(lineItems, "\n"))
+				if len(findings) == 0 {
+					fmt.Println("✅ No vulnerabilities found")
+				} else {
+					tree := newFindingsTree(findings)
+					fmt.Println(tree.render())
+				}
 			}
 
 			return nil
@@ -47,17 +51,91 @@ func Scan() *cobra.Command {
 	return cmd
 }
 
-func renderFinding(f scan.Finding) string {
-	return fmt.Sprintf(
-		"%s (%s) @ %s %s %s (%s): %s",
-		f.Package,
-		f.Type,
-		f.Version,
-		styleSubtle.Render("matched to"),
-		f.VulnerabilityID,
-		strings.Join(f.Aliases, ", "),
-		renderSeverity(f.Severity),
-	)
+type findingsTree struct {
+	findingsByPackageByLocation map[string]map[string][]*scan.Finding
+	packagesByID                map[string]scan.Package
+}
+
+func newFindingsTree(findings []*scan.Finding) *findingsTree {
+	tree := make(map[string]map[string][]*scan.Finding)
+	packagesByID := make(map[string]scan.Package)
+
+	for _, f := range findings {
+		loc := f.Package.Location
+		packageID := f.Package.ID
+		packagesByID[packageID] = f.Package
+
+		if _, ok := tree[loc]; !ok {
+			tree[loc] = make(map[string][]*scan.Finding)
+		}
+
+		tree[loc][packageID] = append(tree[loc][packageID], f)
+	}
+
+	return &findingsTree{
+		findingsByPackageByLocation: tree,
+		packagesByID:                packagesByID,
+	}
+}
+
+func (t findingsTree) render() string {
+	locations := lo.Keys(t.findingsByPackageByLocation)
+	sort.Strings(locations)
+
+	var lines []string
+	for i, location := range locations {
+		var treeStem, verticalLine string
+		if i == len(locations)-1 {
+			treeStem = "└── "
+			verticalLine = " "
+		} else {
+			treeStem = "├── "
+			verticalLine = "│"
+		}
+
+		line := treeStem + fmt.Sprintf("📄 %s", location)
+		lines = append(lines, line)
+
+		packageIDs := lo.Keys(t.findingsByPackageByLocation[location])
+		packages := lo.Map(packageIDs, func(id string, _ int) scan.Package {
+			return t.packagesByID[id]
+		})
+
+		sort.SliceStable(packages, func(i, j int) bool {
+			return packages[i].Name < packages[j].Name
+		})
+
+		for _, pkg := range packages {
+			line := fmt.Sprintf(
+				"%s       📦 %s %s %s",
+				verticalLine,
+				pkg.Name,
+				pkg.Version,
+				styleSubtle.Render("("+pkg.Type+")"),
+			)
+			lines = append(lines, line)
+
+			findings := t.findingsByPackageByLocation[location][pkg.ID]
+			sort.SliceStable(findings, func(i, j int) bool {
+				return findings[i].Vulnerability.ID < findings[j].Vulnerability.ID
+			})
+
+			for _, f := range findings {
+				line := fmt.Sprintf(
+					"%s           %s %s%s",
+					verticalLine,
+					renderSeverity(f.Vulnerability.Severity),
+					renderVulnerabilityID(f.Vulnerability),
+					renderFixedIn(f.Vulnerability),
+				)
+				lines = append(lines, line)
+			}
+		}
+
+		lines = append(lines, verticalLine)
+	}
+
+	return strings.Join(lines, "\n")
 }
 
 func renderSeverity(severity string) string {
@@ -75,6 +153,54 @@ func renderSeverity(severity string) string {
 	default:
 		return severity
 	}
+}
+
+func renderVulnerabilityID(vuln scan.Vulnerability) string {
+	var cveID string
+
+	for _, alias := range vuln.Aliases {
+		if strings.HasPrefix(alias, "CVE-") {
+			cveID = alias
+			break
+		}
+	}
+
+	if cveID == "" {
+		return hyperlinkVulnerabilityID(vuln.ID)
+	}
+
+	return fmt.Sprintf(
+		"%s %s",
+		hyperlinkVulnerabilityID(cveID),
+
+		styleSubtle.Render(hyperlinkVulnerabilityID(vuln.ID)),
+	)
+}
+
+var termSupportsHyperlinks = termlink.SupportsHyperlinks()
+
+func hyperlinkVulnerabilityID(id string) string {
+	if !termSupportsHyperlinks {
+		return id
+	}
+
+	switch {
+	case strings.HasPrefix(id, "CVE-"):
+		return termlink.Link(id, fmt.Sprintf("https://nvd.nist.gov/vuln/detail/%s", id))
+
+	case strings.HasPrefix(id, "GHSA-"):
+		return termlink.Link(id, fmt.Sprintf("https://github.com/advisories/%s", id))
+	}
+
+	return id
+}
+
+func renderFixedIn(vuln scan.Vulnerability) string {
+	if vuln.FixedVersion == "" {
+		return ""
+	}
+
+	return fmt.Sprintf(" fixed in %s", vuln.FixedVersion)
 }
 
 var (
--- pkg/scan/apk.go
+++ pkg/scan/apk.go
@@ -5,16 +5,19 @@ import (
 	"io"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/adrg/xdg"
 	"github.com/anchore/grype/grype"
 	"github.com/anchore/grype/grype/db"
+	v5 "github.com/anchore/grype/grype/db/v5"
 	"github.com/anchore/grype/grype/match"
 	grypePkg "github.com/anchore/grype/grype/pkg"
 	"github.com/anchore/grype/grype/store"
 	"github.com/anchore/grype/grype/vulnerability"
 	"github.com/anchore/syft/syft"
+	"github.com/anchore/syft/syft/file"
 	"github.com/anchore/syft/syft/pkg/cataloger"
 	"github.com/anchore/syft/syft/source"
 	"github.com/samber/lo"
@@ -49,7 +52,7 @@ var syftCatalogersEnabled = []string{
 }
 
 // APK scans an APK file for vulnerabilities.
-func APK(file io.Reader) ([]*Finding, error) {
+func APK(f io.Reader) ([]*Finding, error) {
 	// Create a temp directory to house the unpacked APK file
 	tempDir, err := os.MkdirTemp("", "wolfictl-scan-*")
 	if err != nil {
@@ -58,7 +61,7 @@ func APK(file io.Reader) ([]*Finding, error) {
 	defer os.RemoveAll(tempDir)
 
 	// Unpack apk to temp directory
-	err = tar.Untar(file, tempDir)
+	err = tar.Untar(f, tempDir)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unpack apk file: %w", err)
 	}
@@ -84,13 +87,13 @@ func APK(file io.Reader) ([]*Finding, error) {
 
 	syftPkgs := packageCollection.Sorted()
 
-	store, _, dbCloser, err := grype.LoadVulnerabilityDB(grypeDBConfig, true)
+	datastore, _, dbCloser, err := grype.LoadVulnerabilityDB(grypeDBConfig, true)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load vulnerability database: %w", err)
 	}
 	defer dbCloser.Close()
 
-	matcher := grype.DefaultVulnerabilityMatcher(*store)
+	matcher := grype.DefaultVulnerabilityMatcher(*datastore)
 	sourceDescription := src.Describe()
 	grypePkgs := grypePkg.FromPackages(syftPkgs, grypePkg.SynthesisConfig{GenerateMissingCPEs: false})
 	matchesCollection, _, err := matcher.FindMatches(grypePkgs, grypePkg.Context{
@@ -104,8 +107,10 @@ func APK(file io.Reader) ([]*Finding, error) {
 	matches := matchesCollection.Sorted()
 
 	var findings []*Finding
-	for _, m := range matches {
-		finding, err := mapMatchToFinding(m, store)
+	for i := range matches {
+		m := matches[i]
+
+		finding, err := mapMatchToFinding(m, datastore)
 		if err != nil {
 			return nil, fmt.Errorf("failed to map match to finding: %w", err)
 		}
@@ -117,23 +122,34 @@ func APK(file io.Reader) ([]*Finding, error) {
 
 // Finding represents a vulnerability finding for a single package.
 type Finding struct {
-	Package         string
-	Version         string
-	Type            string
-	VulnerabilityID string
-	Severity        string
-	Aliases         []string
+	Package       Package
+	Vulnerability Vulnerability
 }
 
-func mapMatchToFinding(m match.Match, store *store.Store) (*Finding, error) {
-	metadata, err := store.MetadataProvider.GetMetadata(m.Vulnerability.ID, m.Vulnerability.Namespace)
+type Package struct {
+	ID       string
+	Name     string
+	Version  string
+	Type     string
+	Location string
+}
+
+type Vulnerability struct {
+	ID           string
+	Severity     string
+	Aliases      []string
+	FixedVersion string
+}
+
+func mapMatchToFinding(m match.Match, datastore *store.Store) (*Finding, error) {
+	metadata, err := datastore.MetadataProvider.GetMetadata(m.Vulnerability.ID, m.Vulnerability.Namespace)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get metadata for vulnerability %s: %w", m.Vulnerability.ID, err)
 	}
 
 	var relatedMetadatas []*vulnerability.Metadata
 	for _, relatedRef := range m.Vulnerability.RelatedVulnerabilities {
-		relatedMetadata, err := store.MetadataProvider.GetMetadata(relatedRef.ID, relatedRef.Namespace)
+		relatedMetadata, err := datastore.MetadataProvider.GetMetadata(relatedRef.ID, relatedRef.Namespace)
 		if err != nil {
 			return nil, fmt.Errorf("unable to get metadata for related vulnerability %s: %w", relatedRef.ID, err)
 		}
@@ -144,14 +160,33 @@ func mapMatchToFinding(m match.Match, store *store.Store) (*Finding, error) {
 		return m.ID
 	})
 
+	locations := lo.Map(m.Package.Locations.ToSlice(), func(l file.Location, _ int) string {
+		return "/" + l.RealPath
+	})
+
 	f := &Finding{
-		Package:         m.Package.Name,
-		Version:         m.Package.Version,
-		Type:            string(m.Package.Type),
-		VulnerabilityID: m.Vulnerability.ID,
-		Severity:        metadata.Severity,
-		Aliases:         aliases,
+		Package: Package{
+			ID:       string(m.Package.ID),
+			Name:     m.Package.Name,
+			Version:  m.Package.Version,
+			Type:     string(m.Package.Type),
+			Location: strings.Join(locations, ", "),
+		},
+		Vulnerability: Vulnerability{
+			ID:           m.Vulnerability.ID,
+			Severity:     metadata.Severity,
+			Aliases:      aliases,
+			FixedVersion: getFixedVersion(m.Vulnerability),
+		},
 	}
 
 	return f, nil
+}
+
+func getFixedVersion(vuln vulnerability.Vulnerability) string {
+	if vuln.Fix.State != v5.FixedState {
+		return ""
+	}
+
+	return strings.Join(vuln.Fix.Versions, ", ")
 }
