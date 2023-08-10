5738892e83d9dab65a372e3f48effad93e0329a7
Dan Luhring <dluhring@chainguard.dev>
2023-08-10 08:49:50 -0400

    chore: remove deprecated sync flag
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- pkg/cli/vex.go
+++ pkg/cli/vex.go
@@ -8,7 +8,7 @@ import (
 
 func VEX() *cobra.Command {
 	cmd := &cobra.Command{
-		Deprecated: "This command does nothing, and will be revmoved in a future version.",
+		Deprecated: "This command does nothing, and will be removed in a future version.",
 		Use:        "vex",
 		Short:      "Tools to generate VEX statements for Wolfi packages and images",
 		Long: `wolfictl vex: Tools to generate VEX statements for Wolfi packages and images
