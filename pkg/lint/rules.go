1a8be7c69d77baed3b0865624300a92928ddbf6c
Jason Hall <jason@chainguard.dev>
2024-01-27 18:32:08 -0500

    make invalid SPDX identifier an error
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/lint/rules.go
+++ pkg/lint/rules.go
@@ -395,7 +395,7 @@ var AllRules = func(l *Linter) Rules { //nolint:gocyclo
 		{
 			Name:        "valid-spdx-license",
 			Description: "every package should have a valid SPDX license",
-			Severity:    SeverityWarning, // TODO(jason): Make this an error.
+			Severity:    SeverityError,
 			LintFunc: func(config config.Configuration) error {
 				for _, c := range config.Package.Copyright {
 					if c.License == "" {
