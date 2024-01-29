018e9fbc00a43f113f3024002c50d737575b1d41
James Petersen <jpetersenames@gmail.com>
2024-01-29 16:21:03 -0700

    more table testing
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- pkg/ruby/discover_test.go
+++ pkg/ruby/discover_test.go
@@ -19,30 +19,31 @@ func TestIsRubyPackage(t *testing.T) {
 			},
 		},
 	}
+	tests := map[string]struct {
+		want  bool
+		input string
+	}{
+		// success cases
+		"three_two":     {want: true, input: "ruby-3.2"},
+		"three_two_dev": {want: true, input: "ruby-3.2-dev"},
+
+		// failure cases
+		"fail_two_two":            {want: false, input: "ruby-2.2"},
+		"fail_three_twowo":        {want: false, input: "ruby-3.22"},
+		"fail_threethree_two":     {want: false, input: "ruby-33.2"},
+		"fail_two_two_dev":        {want: false, input: "ruby-2.2-dev"},
+		"fail_three_twowo_dev":    {want: false, input: "ruby-3.22-dev"},
+		"fail_threethree_two_dev": {want: false, input: "ruby-33.2-dev"},
+	}
+
 	o := Options{
 		RubyVersion: "3.2",
 	}
 
-	shouldBeTrue := []string{
-		"ruby-3.2",
-		"ruby-3.2-dev",
-	}
-	for _, pkg := range shouldBeTrue {
-		testConfig.Environment.Contents.Packages[0] = pkg
-		assert.Truef(t, o.isRubyPackage(testConfig), "%s should be true", pkg)
-	}
-
-	shouldBeFalse := []string{
-		"ruby-2.2",
-		"ruby-3.22",
-		"ruby-33.2",
-		"ruby-2.2-dev",
-		"ruby-3.22-dev",
-		"ruby-33.2-dev",
-	}
-	for _, pkg := range shouldBeFalse {
-		testConfig.Environment.Contents.Packages[0] = pkg
-		assert.Falsef(t, o.isRubyPackage(testConfig), "%s should be false", pkg)
+	for name, tc := range tests {
+		testConfig.Environment.Contents.Packages[0] = tc.input
+		got := o.isRubyPackage(testConfig)
+		assert.Equalf(t, tc.want, got, "%s wanted: %s got: %s", name, tc.want, got)
 	}
 }
 
@@ -58,37 +59,33 @@ func TestParseRepo(t *testing.T) {
 	}
 
 	uri := "https://github.com/brianmario/charlock_holmes/archive/refs/tags/v${{package.version}}.tar.gz"
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "fetch",
-		With: map[string]string{
-			"uri": uri,
-		},
-	}
-	assert.Equal(t, uri, parseRepo(&testConfig))
-
 	repository := "https://github.com/brianmario/charlock_holmes.git"
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "git-checkout",
-		With: map[string]string{
-			"repository": repository,
-		},
+	tests := map[string]struct {
+		want string
+		uses string
+		with map[string]string
+	}{
+		"fetch_uri":     {want: uri, uses: "fetch", with: map[string]string{"uri": uri}},
+		"checkout_repo": {want: repository, uses: "git-checkout", with: map[string]string{"repository": repository}},
+		"other_junk":    {want: "", uses: "something-else", with: map[string]string{"junk": ""}},
 	}
-	assert.Equal(t, repository, parseRepo(&testConfig))
 
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "something-else",
-		With: map[string]string{
-			"junk": "",
-		},
+	for name, tc := range tests {
+		testConfig.Config.Pipeline[0] = config.Pipeline{
+			Uses: tc.uses,
+			With: tc.with,
+		}
+		got := parseRepo(&testConfig)
+		assert.Equalf(t, tc.want, got, "%s wanted: %s got %s", name, tc.want, got)
 	}
-	assert.Equal(t, "", parseRepo(&testConfig))
 }
 
 func TestParseRef(t *testing.T) {
+	pkgVersion := "0.0.0"
 	testConfig := melange.Packages{
 		Config: config.Configuration{
 			Package: config.Package{
-				Version: "0.0.0",
+				Version: pkgVersion,
 			},
 			Pipeline: []config.Pipeline{
 				{
@@ -97,48 +94,25 @@ func TestParseRef(t *testing.T) {
 			},
 		},
 	}
-	want := "v0.0.0"
 
-	uri := "https://github.com/brianmario/charlock_holmes/archive/refs/tags/v${{package.version}}.tar.gz"
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "fetch",
-		With: map[string]string{
-			"uri": uri,
-		},
+	tests := map[string]struct {
+		want string
+		uses string
+		with map[string]string
+	}{
+		"fetch_uri":       {want: "v" + pkgVersion, uses: "fetch", with: map[string]string{"uri": "https://github.com/brianmario/charlock_holmes/archive/refs/tags/v${{package.version}}.tar.gz"}},
+		"checkout_tag":    {want: "v" + pkgVersion, uses: "git-checkout", with: map[string]string{"tag": "v${{package.version}}"}},
+		"checkout_branch": {want: "v" + pkgVersion, uses: "git-checkout", with: map[string]string{"branch": "v${{package.version}}"}},
+		"checkout_junk":   {want: "", uses: "git-checkout", with: map[string]string{"junk": ""}},
+		"other_junk":      {want: "", uses: "something-else", with: map[string]string{"junk": ""}},
 	}
-	assert.Equal(t, want, parseRef(&testConfig))
 
-	tag := "v${{package.version}}"
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "git-checkout",
-		With: map[string]string{
-			"tag": tag,
-		},
+	for name, tc := range tests {
+		testConfig.Config.Pipeline[0] = config.Pipeline{
+			Uses: tc.uses,
+			With: tc.with,
+		}
+		got := parseRef(&testConfig)
+		assert.Equalf(t, tc.want, got, "%s wanted: %s got %s", name, tc.want, got)
 	}
-	assert.Equal(t, want, parseRef(&testConfig))
-
-	branch := "v${{package.version}}"
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "git-checkout",
-		With: map[string]string{
-			"branch": branch,
-		},
-	}
-	assert.Equal(t, want, parseRef(&testConfig))
-
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "git-checkout",
-		With: map[string]string{
-			"junk": "",
-		},
-	}
-	assert.Equal(t, "", parseRef(&testConfig))
-
-	testConfig.Config.Pipeline[0] = config.Pipeline{
-		Uses: "something-else",
-		With: map[string]string{
-			"junk": "",
-		},
-	}
-	assert.Equal(t, "", parseRef(&testConfig))
 }
