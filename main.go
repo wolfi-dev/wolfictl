b167114d35c59cd0e2a124dedf925ba07c558dd5
Dan Luhring <dluhring@chainguard.dev>
2024-05-20 20:31:37 -0400

    fix: command errors are errors
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"context"
-	"log"
 	"os"
 	"os/signal"
 
+	"github.com/chainguard-dev/clog"
 	"github.com/wolfi-dev/wolfictl/pkg/cli"
 )
 
 func main() {
-	if err := mainE(context.Background()); err != nil {
-		log.Fatalf("error during command execution: %v", err)
+	ctx := context.Background()
+	if err := mainE(ctx); err != nil {
+		clog.FromContext(ctx).Error(err.Error())
+		os.Exit(1)
 	}
 }
 
