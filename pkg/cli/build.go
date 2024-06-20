1ce163648bc4ab64c358d49b71f784ceade4129e
Jason Hall <jason@chainguard.dev>
2024-06-20 15:59:15 -0400

    open up bundle code for reuse elsewhere
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/cli/build.go
+++ pkg/cli/build.go
@@ -129,7 +129,7 @@ func cmdBuild() *cobra.Command {
 				}
 				cfg.gcs = client
 
-				return buildBundles(ctx, &cfg, cfg.bundle)
+				return BuildBundles(ctx, &cfg, cfg.bundle)
 			}
 
 			return buildAll(ctx, &cfg, args)
@@ -321,7 +321,7 @@ type buildResult struct {
 	Error    error         `json:"error,omitempty"`
 }
 
-func buildBundles(ctx context.Context, cfg *global, ref string) error {
+func BuildBundles(ctx context.Context, cfg *global, ref string) error {
 	var eg errgroup.Group
 
 	bundles, err := bundle.Pull(ref)
