8c571ab17e687599492e2ff7b81cd81488b84b91
Jason Hall <jason@chainguard.dev>
2024-06-20 16:00:25 -0400

    fix stuff
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- pkg/cli/build.go
+++ pkg/cli/build.go
@@ -47,8 +47,8 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 
+	"github.com/wolfi-dev/wolfictl/pkg/bundle"
 	"github.com/wolfi-dev/wolfictl/pkg/dag"
-	"github.com/wolfi-dev/wolfictl/pkg/internal/bundle"
 	"github.com/wolfi-dev/wolfictl/pkg/tar"
 )
 
