0c2d7f9d796a249956c8456252dba9f44c7ed622
Dan Luhring <dluhring@chainguard.dev>
2025-04-01 17:33:03 -0400

    feat(adv): add FSPutter implementation and complete Putter docs
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

---
+++ pkg/advisory/fs_putter.go
@@ -0,0 +1,184 @@
+package advisory
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"sort"
+
+	"github.com/chainguard-dev/yam/pkg/yam/formatted"
+	v2 "github.com/wolfi-dev/wolfictl/pkg/configs/advisory/v2"
+	"github.com/wolfi-dev/wolfictl/pkg/configs/rwfs"
+)
+
+// DocumentEncoder writes a Document to an io.Writer in a specific format.
+type DocumentEncoder func(w io.Writer, doc v2.Document) error
+
+// NewYamDocumentEncoder creates a new DocumentEncoder that uses yan
+// (https://github.com/chainguard-dev/yam) to encode the document as YAML using
+// the specified formatting options.
+func NewYamDocumentEncoder(opts formatted.EncodeOptions) DocumentEncoder {
+	return func(w io.Writer, doc v2.Document) error {
+		enc := formatted.NewEncoder(w)
+
+		var err error
+		enc, err = enc.UseOptions(opts)
+		if err != nil {
+			return fmt.Errorf("using yam options: %w", err)
+		}
+
+		if err := enc.Encode(doc); err != nil {
+			return fmt.Errorf("encoding document: %w", err)
+		}
+
+		return nil
+	}
+}
+
+// FSPutter is an implementation of Putter that creates or updates an advisory
+// using the given Request, operating on a `.advisories.yaml` file in the given
+// filesystem.
+type FSPutter struct {
+	fsys        rwfs.FS
+	enc         DocumentEncoder
+	idGenerator IDGenerator
+}
+
+// NewFSPutter creates and returns a new FSPutter that updates advisory data in
+// the given filesystem using the provided FileEncoder to marshal and format the
+// file data.
+func NewFSPutter(fsys rwfs.FS, enc DocumentEncoder) *FSPutter {
+	return &FSPutter{
+		fsys:        fsys,
+		enc:         enc,
+		idGenerator: DefaultIDGenerator,
+	}
+}
+
+func (p FSPutter) Upsert(_ context.Context, request Request) (string, error) {
+	if request.Package == "" {
+		return "", ErrEmptyPackage
+	}
+
+	// The advisories file might exist, or not. If it does exist, the advisory
+	// itself might exist, or not.
+
+	advisoryFileHadExisted := false
+
+	advFileName := fmt.Sprintf("%s.advisories.yaml", request.Package)
+	f, err := p.fsys.Open(advFileName)
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return "", fmt.Errorf("opening advisory file %q: %w", advFileName, err)
+	}
+
+	var doc *v2.Document
+	if err == nil {
+		advisoryFileHadExisted = true
+
+		doc, err = v2.DecodeDocument(f)
+		if err != nil {
+			return "", fmt.Errorf("decoding advisory file %q: %w", advFileName, err)
+		}
+	} else {
+		doc = &v2.Document{
+			Package: v2.Package{
+				Name: request.Package,
+			},
+		}
+	}
+
+	if advisoryFileHadExisted {
+		// Done reading. Depending on how far we get in the logic, we'll later open the
+		// file as writeable (if needed).
+		if err := f.Close(); err != nil {
+			return "", fmt.Errorf("closing advisory file %q: %w", advFileName, err)
+		}
+	}
+
+	// We set the schema to current whenever we're operating on the file.
+	doc.SchemaVersion = v2.SchemaVersion
+
+	// Find or create the advisory
+	var advisory *v2.Advisory
+	if reqID := request.AdvisoryID; reqID != "" {
+		adv, exists := doc.Advisories.Get(reqID)
+		if exists {
+			// We'll be updating this existing advisory.
+			advisory = &adv
+		} else {
+			return "", fmt.Errorf("advisory ID %q not found for package %q", reqID, request.Package)
+		}
+	} else {
+		adv, exists := doc.Advisories.GetByAnyVulnerability(request.Aliases...)
+		if exists {
+			// We'll be updating this existing advisory.
+			advisory = &adv
+		} else {
+			// We'll be creating a new advisory.
+			newID, err := p.idGenerator.GenerateCGAID()
+			if err != nil {
+				return "", fmt.Errorf("generating CGA ID when creating new advisory: %w", err)
+			}
+			advisory = &v2.Advisory{
+				ID: newID,
+			}
+		}
+	}
+
+	// Union the alias lists.
+	updatedAliases := union(advisory.Aliases, request.Aliases)
+	advisory.Aliases = updatedAliases
+
+	if !request.Event.IsZero() {
+		advisory.Events = append(advisory.Events, request.Event)
+	}
+
+	doc.Advisories = doc.Advisories.Upsert(advisory.ID, *advisory)
+
+	// Write the updated document back to the file system
+
+	var w rwfs.File
+	if advisoryFileHadExisted {
+		w, err = p.fsys.OpenAsWritable(advFileName)
+		if err != nil {
+			return "", fmt.Errorf("opening advisory file %q as writable: %w", advFileName, err)
+		}
+	} else {
+		w, err = p.fsys.Create(advFileName)
+		if err != nil {
+			return "", fmt.Errorf("creating advisory file %q: %w", advFileName, err)
+		}
+	}
+
+	if err := p.fsys.Truncate(advFileName, 0); err != nil {
+		return "", fmt.Errorf("truncating advisory file %q (prior to writing to it): %w", advFileName, err)
+	}
+
+	if err := p.enc(w, *doc); err != nil {
+		return "", fmt.Errorf("encoding advisory file %q with YAML: %w", advFileName, err)
+	}
+
+	if err := w.Close(); err != nil {
+		return "", fmt.Errorf("closing advisory file %q: %w", advFileName, err)
+	}
+
+	return advisory.ID, nil
+}
+
+func union(slice1, slice2 []string) []string {
+	m := make(map[string]struct{})
+	for _, s := range slice1 {
+		m[s] = struct{}{}
+	}
+	for _, s := range slice2 {
+		m[s] = struct{}{}
+	}
+	result := make([]string, 0, len(m))
+	for s := range m {
+		result = append(result, s)
+	}
+	sort.Strings(result)
+	return result
+}
---
+++ pkg/advisory/fs_putter_test.go
@@ -0,0 +1,149 @@
+package advisory
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/chainguard-dev/yam/pkg/yam/formatted"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	v2 "github.com/wolfi-dev/wolfictl/pkg/configs/advisory/v2"
+	"github.com/wolfi-dev/wolfictl/pkg/configs/rwfs/os/testerfs"
+)
+
+func TestFSPutter_Upsert(t *testing.T) {
+	ctx := t.Context()
+	testTime := v2.Timestamp(time.Date(2022, 9, 15, 2, 40, 18, 0, time.UTC))
+
+	cases := []struct {
+		name                 string
+		req                  Request
+		skipFsysConstruction bool // don't bother with a test fsys since we expect a failure before fsys access
+		expectedID           string
+		assertErr            assert.ErrorAssertionFunc
+	}{
+		{
+			name: "no package name",
+			req: Request{
+				Package: "",
+			},
+			skipFsysConstruction: true,
+			assertErr:            assert.Error,
+		},
+		{
+			name: "package name with nonexistent advisory ID",
+			req: Request{
+				Package:    "foo",
+				AdvisoryID: "CGA-4444-4444-4444",
+			},
+			assertErr: assert.Error,
+		},
+		{
+			name: "no-op update",
+			req: Request{
+				Package:    "foo",
+				AdvisoryID: "CGA-2222-2222-2222",
+				Aliases:    nil,
+				Event:      v2.Event{},
+			},
+			expectedID: "CGA-2222-2222-2222",
+			assertErr:  assert.NoError,
+		},
+		{
+			name: "create when no document exists",
+			req: Request{
+				Package: "foo",
+				Aliases: []string{"CVE-2020-8927"},
+				Event: v2.Event{
+					Timestamp: testTime,
+					Type:      v2.EventTypeFixed,
+					Data: v2.Fixed{
+						FixedVersion: "1.0.9-r0",
+					},
+				},
+			},
+			expectedID: "CGA-2222-2222-2222",
+			assertErr:  assert.NoError,
+		},
+		{
+			name: "create when document exists but not advisory",
+			req: Request{
+				Package: "foo",
+				Aliases: []string{"CVE-2020-8927"},
+				Event: v2.Event{
+					Timestamp: testTime,
+					Type:      v2.EventTypeFixed,
+					Data: v2.Fixed{
+						FixedVersion: "1.0.9-r0",
+					},
+				},
+			},
+			expectedID: "CGA-2222-2222-2222",
+			assertErr:  assert.NoError,
+		},
+		{
+			name: "update",
+			req: Request{
+				Package:    "foo",
+				AdvisoryID: "CGA-2222-2222-2222",
+				Aliases:    []string{"CVE-2020-8927"},
+				Event: v2.Event{
+					Timestamp: testTime,
+					Type:      v2.EventTypePendingUpstreamFix,
+					Data: v2.PendingUpstreamFix{
+						Note: "this is a note",
+					},
+				},
+			},
+			expectedID: "CGA-2222-2222-2222",
+			assertErr:  assert.NoError,
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			testDir := filepath.Join("testdata", "fs_putter", strings.ReplaceAll(tt.name, " ", "-"))
+
+			var fsys *testerfs.FS
+			var err error
+			if !tt.skipFsysConstruction {
+				fsys, err = testerfs.New(os.DirFS(testDir))
+				require.NoError(t, err)
+			}
+
+			var enc DocumentEncoder = func(w io.Writer, doc v2.Document) error {
+				encoder := formatted.NewEncoder(w)
+
+				// Set up the formatting options we expect
+				encoder = encoder.SetIndent(2)
+				encoder, err = encoder.SetGapExpressions(".", ".advisories")
+				require.NoError(t, err)
+
+				return encoder.Encode(doc)
+			}
+
+			p := &FSPutter{
+				fsys:        fsys,
+				enc:         enc,
+				idGenerator: StaticIDGenerator{ID: "CGA-2222-2222-2222"},
+			}
+
+			id, err := p.Upsert(ctx, tt.req)
+			tt.assertErr(t, err)
+
+			if tt.expectedID != id {
+				t.Errorf("expected ID result to be %q, got %q", tt.expectedID, id)
+			}
+
+			if err == nil {
+				if diff := fsys.DiffAll(); diff != "" {
+					t.Errorf("filesystem in an unexpected state (-want +got):\n%s", diff)
+				}
+			}
+		})
+	}
+}
