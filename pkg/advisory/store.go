412b1cf80b143451e9194639fbc9bf7e516d0aff
Dan Luhring <dluhring@chainguard.dev>
2025-04-11 08:56:15 -0400

    refactor(scan): update filtering to use adv getter abstraction
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- pkg/advisory/store.go
+++ pkg/advisory/store.go
@@ -54,3 +54,19 @@ type Putter interface {
 	// returned.
 	Upsert(ctx context.Context, request Request) (string, error)
 }
+
+// MapByVulnID maps the given advisories by their vulnerability ID, creating a
+// pre-indexed collection of advisories for performant lookup. The map keys are
+// the vulnerability IDs, and the values are pointers to the corresponding
+// PackageAdvisory structs.
+func MapByVulnID(advisories []v2.PackageAdvisory) map[string]*v2.PackageAdvisory {
+	advsByAlias := make(map[string]*v2.PackageAdvisory, len(advisories)) // even though we'll exceed this capacity if there are multiple aliases.
+
+	for _, adv := range advisories {
+		for _, alias := range adv.Aliases {
+			advsByAlias[alias] = &adv
+		}
+	}
+
+	return advsByAlias
+}
